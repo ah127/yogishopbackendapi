30ad46b025ffd20fbf05ce89fdb757de1aaa2bec
ah127 <kumnaaryongi904.ky@gmail.com>
2020-07-11 07:57:30 +0545

    yogis shop backend with all setup is added

---
+++ migrations/20190630234243_items_table.js
@@ -0,0 +1,18 @@
+
+    exports.up = function(knex, Promise) {
+        return knex.schema
+              .createTable('items', function(table) {
+                  table.increments('id');
+                  table.string('name', 255).notNullable();
+                  table.double('price', 255).notNullable();
+                  table.string('imageName', 255).notNullable();
+                  table.string('module', 255).notNullable();
+              })
+      };
+      
+      exports.down = function(knex, Promise) {
+        return knex.schema.dropTable('items')
+      };
+      
+  
+
